43d851e177d0c4bceac96b830a43f097be9c01de
Alexander Chekel <alexandr.chekel@gmail.com>
2022-01-28 16:36:48 +0300

    Added gallery dismissal callback & default `GalleryZoomTransitionDelegate` implementation

---
+++ Sources/GalleryMediaPresentable.swift
@@ -0,0 +1,46 @@
+//
+//  GalleryMediaPresentable
+//  LegacyGallery
+//
+//  Created by Alexander Chekel on 28.01.2022.
+//
+
+import UIKit
+
+/// General implementation of `GalleryZoomTransitionDelegate` to ease up the basic usage.
+public protocol GalleryMediaPresentable: UIViewController, GalleryZoomTransitionDelegate {
+    /// Transition source image view provider.
+    var sourceImageViewProvider: () -> UIImageView { get set }
+}
+
+public extension GalleryMediaPresentable {
+    private var transitionImageViewFrame: CGRect {
+        let imageView = sourceImageViewProvider()
+        return imageView.superview?.convert(imageView.frame, to: nil) ?? .zero
+    }
+
+    var zoomTransition: GalleryZoomTransition? {
+        nil
+    }
+
+    var zoomTransitionInteractionController: UIViewControllerInteractiveTransitioning? {
+        nil
+    }
+
+    var zoomTransitionAnimatingView: UIView? {
+        let sourceImageView = sourceImageViewProvider()
+        let transitionImageView = UIImageView(image: sourceImageView.image)
+        transitionImageView.clipsToBounds = true
+        transitionImageView.layer.cornerRadius = sourceImageView.layer.cornerRadius
+        transitionImageView.frame = transitionImageViewFrame
+        return transitionImageView
+    }
+
+    func zoomTransitionHideViews(hide: Bool) {
+        sourceImageViewProvider().alpha = hide ? 1 : 0
+    }
+
+    func zoomTransitionDestinationFrame(for view: UIView, frame: CGRect) -> CGRect {
+        transitionImageViewFrame
+    }
+}
